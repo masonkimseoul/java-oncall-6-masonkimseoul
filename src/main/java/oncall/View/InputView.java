034c8c39881ca6726b841d6ae8c565a7cfcc8466
masonkimseoul <masonkimseoul@gmail.com>
2023-12-16 15:24:40 +0900

    feat : InputView 추가
    입력 기능 관리

---
+++ src/main/java/oncall/View/InputView.java
@@ -0,0 +1,31 @@
+package oncall.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String SEPARATOR = ",";
+
+    public static String[] getStartInfo() {
+        String[] startInfo = Console.readLine().split(SEPARATOR);
+
+        try {
+            //Validation
+            return startInfo;
+        } catch (IllegalArgumentException e) {
+            System.out.print(e.getMessage());
+            return getStartInfo();
+        }
+    }
+
+    private static String[] getWeekDayWorkersNames() {
+        String[] weekDayWorkersNames = Console.readLine().split(SEPARATOR);
+        //Validation
+        return weekDayWorkersNames;
+    }
+
+    private static String[] getOffDayWorkersNames() {
+        String[] offDayWorkersNames = Console.readLine().split(SEPARATOR);
+        //Validation
+        return offDayWorkersNames;
+    }
+}
